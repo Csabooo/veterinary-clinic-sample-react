ac1b558994a31e097163596339f2fa2956912b8d
Müller Csaba <csaba.muller@hotmail.com>
2023-08-28 20:19:41 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        alice: ["alice", "ui-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
